693e56f92ddca9e71e1d4ab620bf313a1d819e5d
Archila <ivanestarchila@gmail.com>
2020-01-30 13:16:15 -0600

    Agregada paginacion para la vista de destinos

--- src/app/views/panel/destino/destino-index/destino-index.component.ts
+++ src/app/views/panel/destino/destino-index/destino-index.component.ts
@@ -19,6 +19,21 @@ export class DestinoIndexComponent implements OnInit {
   
     @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
+  //Paginacion
+  public currentPage = 1;
+  public length = 0;
+  public pageSize = 5;
+  public sortBy = "id";
+  public sortOrder = "asc";
+
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+  
+  //Busqueda
+  public filterQuery = '';
+  public searchTimeout : any;
+
 
   constructor(private destinoService:DestinoService) { 
     this.getDestinos();
@@ -31,13 +46,48 @@ export class DestinoIndexComponent implements OnInit {
     
   }
 
+  //Parametro: BUsqueda es un termino para buscar
+  //Parametro: Reset si hay que volver a la primera pagina
+  getDestinos(busqueda? : any, reset? : boolean){
+    //Guardamos la pagina actual
+    var page = this.currentPage;    
+    
+    //Si debemos volver a la primer pagina
+    if(reset) {
+        page = 1;
+    }
 
+    //Filtros del destino
+    let filtro =  {
+      page: page,
+      many: this.pageSize,
+      sort_by: this.sortBy,
+      direction: this.sortOrder,
+      ...busqueda
+    }    
 
-  getDestinos(){
-   let filtro={}
-    this.destinoService.getDestinos(filtro).subscribe((datos:any)=>{
-      this.destinos=new MatTableDataSource<any>(datos.data);
+    this.destinoService.getDestinos(filtro).subscribe(
+      (datos:any)=>{
+        this.destinos=new MatTableDataSource<any>(datos.data);
+        this.length = datos.total;
+
+        if (reset){
+          this.currentPage = 1;
+        }
     });
+  }
+
+  //Cambio de pagina y de cantidad de elementos por pagina
+    cambioPagina(event){
+      this.currentPage = event.pageIndex+1;
+      this.pageSize = event.pageSize;
+
+      if (this.filterQuery !== '') {
+        this.getDestinos({nombre : this.filterQuery});
+      }
+      else {
+        this.getDestinos();
+      }
   }
 
 }
